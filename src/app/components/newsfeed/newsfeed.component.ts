b62959ed5907bd0b712e3963e8d321aef4be5da5
Seth Nugent <sethn443@gmail.com>
2017-12-29 15:29:24 -0600

    newsfeed now iterates over array

--- src/app/components/newsfeed/newsfeed.component.ts
+++ src/app/components/newsfeed/newsfeed.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Newsfeed } from "../../shared/models/newsfeed.model";
+import { NEWS } from "../../shared/mock-data";
 
 @Component({
   selector: 'app-newsfeed',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./newsfeed.component.scss']
 })
 export class NewsfeedComponent implements OnInit {
+  newsfeed = NEWS;
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  onNewsDeleted(index) {
+    this.newsfeed.splice(index, 1);
+  }
+  getTopNewsFeed() {
+    return this.newsfeed.filter((item, index) => index >= 0 && index <5 )
+  }
 }
